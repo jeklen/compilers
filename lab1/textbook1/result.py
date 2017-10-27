6377606a0e7410a7cd96e6e86027f265a2d560f9
jeklen <zhql0907@outlook.com>
2017-10-27 22:00:34 +0800

    能够更改大小写，检测16进制，十六进制0x和0X都算，python中0打头算八进制

---
+++ lab1/textbook1/result.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python 
+     
+'''main  fuction'''   
+def  main():
+    a = (0o21 + a) * 2   # 0o21 equals 17
+    b = 0b1001 * 0o37   # 0b1001 equals 9
+    c = a - 55   # a equals 161
+    d = 0101   # 0101 equals 65
+    print a + b - c - d
+'''end'''
+
+if   __name__  ==  '__main__':
+    """    
+    call 
+    main function 
+    """
+    main()  
+
+
